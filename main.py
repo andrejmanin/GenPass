7c5987ce925d7ec409fc8898845d4f64b0fab486
andrew-developer <andrejmanin44@gmail.com>
2024-01-17 20:21:34 +0200

    Added show funk

--- main.py
+++ main.py
@@ -20,11 +20,42 @@ def symbol(upper, lover, nums, specifyc_sym):
         symbols += el
     return symbols
 
-def generate(nums_count):
-    symbols = symbol(True, True, True, True)
+def generate(nums_count, symbols):
     password = ""
     for i in range(nums_count):
         password += random.choice(symbols)
     print(password)
 
-generate(12)
+def show():
+    len = int(input("Enter a len of your pass: "))
+    is_example = str(input("Did you have an example (y/n): "))
+    if is_example == 'n' or is_example == 'N':
+        up = str(input("Upper latters (y/n): "))
+        low = str(input("Lover latters (y/n): "))
+        nums = str(input("Numbers (y/n): "))
+        sp = str(input("Spesific symbols (y/n): "))
+
+        up_sym = False
+        low_sym = False
+        nums_sym = False
+        sp_sym = False
+
+        if up == 'y' or up == 'Y':
+            up_sym = True
+
+        if low == 'y' or low == 'Y':
+            low_sym = True
+
+        if nums == 'y' or nums == 'Y':
+            nums_sym = True
+
+        if sp == 'y' or sp == 'Y':
+            sp_sym = True
+
+        generate(len, symbol(up_sym, low_sym, nums_sym, sp_sym))
+    else:
+        example_let = input("Enter your example letters: ")
+        symbols = [el for el in example_let]
+        generate(len, symbols)
+
+show()
